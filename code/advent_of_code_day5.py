4412b875ccbd5b7f45e819595695583329a8fda5
erinleeryan <erinleeryan@gmail.com>
2020-12-10 08:33:46 -0500

    update day5 for task 2

--- code/advent_of_code_day5.py
+++ code/advent_of_code_day5.py
@@ -7,3 +7,10 @@ for line in data:
     new_data.append(int(line.replace('F', '0').replace('B','1').replace('L', '0').replace('R', '1'),2))
 
 print ('Highest seat num is ', max(new_data))
+
+seats_sorted = np.sort(new_data)
+
+seats_dummy = np.arange(min(new_data), max(new_data))
+
+unassigned = set(seats_dummy) - set(seats_sorted)
+print ('This is your seat: ', unassigned)
